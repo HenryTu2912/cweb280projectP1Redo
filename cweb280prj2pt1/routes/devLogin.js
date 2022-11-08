05d02cd3d1aa0ff65e991fc6395107a012465513
Henry Tu <67282986+hungtp2912@users.noreply.github.com>
2022-11-08 13:20:22 -0600

    Public-Private key Success

--- cweb280prj2pt1/routes/devLogin.js
+++ cweb280prj2pt1/routes/devLogin.js
@@ -1,13 +1,50 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+var fs = require('fs');
 
-router.get('/', (req, res, next)=>{
+router.get('/', checkNotAuthenticated, (req, res, next)=>{
     res.render('login')
 })
 
-router.post('/', passport.authenticate('local',  {
-    successRedirect: '/',
+router.post('/', checkNotAuthenticated, passport.authenticate('local',  {
+    //successRedirect: '/login/welcome',
     failureRedirect: '/login',
-}))
+}), (req, res, next)=>{
+    res.redirect('/login/welcome')
+})
+
+router.get('/welcome', checkAuthenticated, (req, res, next)=>{
+    res.render('welcome', {
+        Title: 'KEY PAGE',
+        publickey: fs.readFileSync('es256public.pem'),
+        privatekey:fs.readFileSync('es256private.key')
+    })
+})
+
+router.post('/welcome', checkAuthenticated, (req, res)=>{
+    var newPublic = req.body.public;
+    var newPrivate = req.body.private;
+    console.log(newPublic);
+    console.log(newPrivate);
+    fs.writeFileSync('es256public.pem', newPublic)
+    fs.writeFileSync('es256private.key', newPrivate)
+    res.render('welcome', {
+        Title: 'SUCCESSFULLY UPDATE KEY',
+    })
+})
+
+function checkAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 module.exports = router
